ac6fe5bfaea256c17313124b603c4d1f81c99c49
KuangKhyng <90085538+KuangKhyng@users.noreply.github.com>
2021-12-11 00:23:10 +0300

    Add files via upload

---
+++ src/ru/mirea/exercise25_26/Stations.java
@@ -0,0 +1,10 @@
+package ru.mirea.exercise25_26;
+
+public class Stations {
+    private String lineId;
+    private String name;
+    public Stations(String lineId, String name) {
+        this.lineId = lineId;
+        this.name = name;
+    }
+}
